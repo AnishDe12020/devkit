0c900177f56cc3386a99a976ad41246e59f8c78c
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-10-21 22:02:25 +0530

    added dark mode

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "@/styles/theme";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Heading } from "@chakra-ui/react";
+import { Heading, IconButton, useColorMode } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@heroicons/react/outline";
 
 const Home: NextPage = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <div>
       <Head>
@@ -15,6 +18,13 @@ const Home: NextPage = () => {
       </Head>
 
       <Heading>DevKit</Heading>
+      <IconButton
+        onClick={toggleColorMode}
+        icon={colorMode === "light" ? <SunIcon /> : <MoonIcon />}
+        variant={"ghost"}
+        ml={8}
+        aria-label={"Toggle Color Mode"}
+      />
     </div>
   );
 };
