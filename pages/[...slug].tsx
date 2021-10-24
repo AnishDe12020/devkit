fdfe3ebed7f95f2fa318a7f7e1e33e2a8a6077e6
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-10-24 15:40:36 +0530

    slug page now matches index page

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,13 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Heading, IconButton, useColorMode } from "@chakra-ui/react";
-import { FiSun, FiMoon } from "react-icons/fi";
 import Sidebar from "@/components/Sidebar";
 import categories from "@/data/categories";
 
 const Home: NextPage = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
-
   return (
     <div>
       <Head>
@@ -19,14 +15,6 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Heading>DevKit</Heading>
-      <IconButton
-        onClick={toggleColorMode}
-        icon={colorMode === "light" ? <FiSun /> : <FiMoon />}
-        variant={"ghost"}
-        ml={8}
-        aria-label={"Toggle Color Mode"}
-      />
       <Sidebar categories={categories} />
     </div>
   );
