dc81a27f57ad5a0637f654d8ab109566b01064a1
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-10-29 16:07:17 +0530

    made a simple gradient generator taking css as input

--- components/Tools/Color/GradientGenerator/index.tsx
+++ components/Tools/Color/GradientGenerator/index.tsx
@@ -1,10 +1,22 @@
-import { Box, Center, Heading } from "@chakra-ui/react";
+import { Box, Center, Heading, Flex, Input } from "@chakra-ui/react";
+import { useState } from "react";
 
 const Gradient = (): JSX.Element => {
+  const [gradientCSS, setGradientCSS] = useState<string>(
+    "linear(to-r, green.200, pink.500)"
+  );
   return (
     <Box>
       <Center>
-        <Heading>Gradient Generator</Heading>
+        <Flex flexDir="column">
+          <Heading>Gradient Generator</Heading>
+          <Box w="100%" h="200px" bgGradient={gradientCSS} mt={12} />
+          <Input
+            mt={8}
+            placeholder="Enter raw css"
+            onChange={e => setGradientCSS(e.target.value)}
+          />
+        </Flex>
       </Center>
     </Box>
   );
