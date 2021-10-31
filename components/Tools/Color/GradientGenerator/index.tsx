736eb76623a3cff36ff1cb4d7a7b96cd50214d3a
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-01 00:13:51 +0530

    added some more inputs to he gradient generator

--- components/Tools/Color/GradientGenerator/index.tsx
+++ components/Tools/Color/GradientGenerator/index.tsx
@@ -1,10 +1,26 @@
-import { Box, Center, Heading, Flex, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Heading,
+  Flex,
+  Input,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from "@chakra-ui/react";
 import { useState } from "react";
 
 const Gradient = (): JSX.Element => {
   const [gradientCSS, setGradientCSS] = useState<string>(
-    "linear(to right, green.200, pink.500)"
+    "linear(to right, #ff0000, #00ff00)"
   );
+  const [gradientDirection, setGradientDirection] = useState<number>(90);
+  const [gradientColors, setGradientColors] = useState<string[]>([
+    "#ff0000",
+    "#00ff00",
+  ]);
+
   return (
     <Box>
       <Center>
@@ -19,9 +35,37 @@ const Gradient = (): JSX.Element => {
           />
           <Input
             mt={8}
-            placeholder="Enter raw css. E.g: linear(to right, green.200, pink.500)"
+            placeholder="Enter raw css. E.g: linear(to right, #ff0000, #00ff00)"
+            value={gradientCSS}
             onChange={e => setGradientCSS(e.target.value)}
           />
+          <Slider
+            mt={4}
+            min={0}
+            max={360}
+            value={gradientDirection}
+            onChange={value => setGradientDirection(value)}
+          >
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+          {gradientColors.map((color, index) => (
+            <Input
+              key={index}
+              mt={8}
+              type="color"
+              value={color}
+              onChange={e =>
+                setGradientColors(
+                  gradientColors.map((c, i) =>
+                    i === index ? e.target.value : c
+                  )
+                )
+              }
+            />
+          ))}
         </Flex>
       </Center>
     </Box>
