7e5c2ed9cb5a31c411e07661d5fe744e823f5377
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-01 00:34:31 +0530

    added ability to add colors and kept values in sync

--- components/Tools/Color/GradientGenerator/index.tsx
+++ components/Tools/Color/GradientGenerator/index.tsx
@@ -8,18 +8,39 @@ import {
   SliderTrack,
   SliderFilledTrack,
   SliderThumb,
+  Button,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Gradient = (): JSX.Element => {
   const [gradientCSS, setGradientCSS] = useState<string>(
-    "linear(to right, #ff0000, #00ff00)"
+    "linear(90deg, #ff0000, #00ff00)"
   );
   const [gradientDirection, setGradientDirection] = useState<number>(90);
   const [gradientColors, setGradientColors] = useState<string[]>([
     "#ff0000",
     "#00ff00",
   ]);
+
+  useEffect(() => {
+    console.log(gradientCSS);
+    setGradientCSS(
+      `linear-gradient(${gradientDirection}deg, ${gradientColors.join(", ")})`
+    );
+  }, [gradientDirection, gradientColors]);
+
+  useEffect(() => {
+    let direction = gradientCSS.split("(")[1].split("deg")[0];
+    let colors = gradientCSS.split("deg")[1].split(",");
+    colors.shift();
+    colors.forEach((color, index) => {
+      color = color.trim();
+      color = color.replace(")", "");
+      colors[index] = color;
+    });
+    setGradientDirection(parseInt(direction));
+    setGradientColors(colors);
+  }, [gradientCSS]);
 
   return (
     <Box>
@@ -66,6 +87,11 @@ const Gradient = (): JSX.Element => {
               }
             />
           ))}
+          <Button
+            onClick={() => setGradientColors([...gradientColors, "#000000"])}
+          >
+            Add Color
+          </Button>
         </Flex>
       </Center>
     </Box>
