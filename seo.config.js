a3c017db89e8fa8026dd90250491c228ba8db5a5
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-09 13:57:00 +0530

    added opengraph title, description and image to default SEO

--- seo.config.js
+++ seo.config.js
@@ -4,6 +4,16 @@ export default {
     locale: "en_US",
     url: "https://devkit.vercel.app/",
     site_name: "DevKit",
+    title: "DevKit",
+    description: "Tools for developers, by developers",
+    images: [
+      {
+        url: "https://i.imgur.com/Opo96rx.png",
+        width: 1200,
+        height: 627,
+        alt: "DevKit OG Image",
+      },
+    ],
   },
   twitter: {
     handle: "@AnishDe12020",
