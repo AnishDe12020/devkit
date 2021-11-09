a0c312e720d94d6b9c297775ad84a0d0d3dbb3c9
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-09 16:52:40 +0530

    added default language

--- next.config.js
+++ next.config.js
@@ -9,5 +9,9 @@ module.exports = withPWA({
     disable: process.env.NODE_ENV === "development",
     buildExcludes: [/middleware-manifest\.json$/],
   },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
   reactStrictMode: true,
 });
