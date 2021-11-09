cdec0dce9174e0cf789da30f5ab72314518e08b2
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-09 13:14:01 +0530

    added default SEO with next seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { DefaultSeo } from "next-seo";
 import theme from "@/styles/theme";
+import SEO from "../seo.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
---
+++ seo.config.js
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://devkit.vercel.app/",
+    site_name: "DevKit",
+  },
+  twitter: {
+    handle: "@AnishDe12020",
+    site: "@AnishDe12020",
+    cardType: "summary_large_image",
+  },
+};
