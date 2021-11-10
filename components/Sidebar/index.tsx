18b34464483611befda327b7a9348fbac15efd3f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-10 14:45:45 +0530

    made the sidebar responsive by making it a drawer on mobile

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,6 +1,19 @@
 import { Category } from "@/data/types";
-import { Box, Heading, IconButton, useColorMode, Flex } from "@chakra-ui/react";
-import { FiSun, FiMoon } from "react-icons/fi";
+import {
+  Box,
+  Heading,
+  IconButton,
+  Flex,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  useDisclosure,
+  useColorMode,
+  useBreakpointValue,
+  DrawerOverlay,
+  DrawerContent,
+} from "@chakra-ui/react";
+import { FiSun, FiMoon, FiMenu, FiX } from "react-icons/fi";
 import { useRouter } from "next/router";
 import HomeLink from "@/components/Sidebar/HomeLink";
 
@@ -18,13 +31,22 @@ const Sidebar = (props: SidebarProps): JSX.Element => {
 
   const { categories, ...otherProps } = props;
 
-  return (
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  type SideBarType = "mobile" | "desktop" | undefined;
+
+  const sideBarType: SideBarType = useBreakpointValue({
+    base: "mobile",
+    md: "desktop",
+    default: "desktop",
+  });
+
+  return sideBarType === "desktop" ? (
     <Box {...otherProps}>
       <Flex alignItems="center">
         <Heading>DevKit</Heading>
         <IconButton
           ml={4}
-          size="md"
           onClick={toggleColorMode}
           icon={colorMode === "light" ? <FiSun /> : <FiMoon />}
           aria-label={"Toggle Color Mode"}
@@ -37,6 +59,45 @@ const Sidebar = (props: SidebarProps): JSX.Element => {
         ))}
       </Flex>
     </Box>
+  ) : (
+    <>
+      <IconButton
+        icon={<FiMenu />}
+        aria-label="Open Sidebar"
+        onClick={onOpen}
+      />
+      <Drawer isOpen={isOpen} onClose={onClose} placement="left" size="full">
+        <DrawerOverlay />
+        <DrawerContent>
+          <Box ml={4} mt={4}>
+            <Flex alignItems="center" justifyContent="space-between">
+              <Flex>
+                <Heading>DevKit</Heading>
+                <IconButton
+                  ml={2}
+                  onClick={toggleColorMode}
+                  icon={colorMode === "light" ? <FiSun /> : <FiMoon />}
+                  aria-label={"Toggle Color Mode"}
+                />
+              </Flex>
+
+              <IconButton
+                mr={4}
+                icon={<FiX />}
+                aria-label="Close Drawer"
+                onClick={onClose}
+              />
+            </Flex>
+            <Flex mt={4} flexDir={"column"}>
+              <HomeLink active={undefined === router.query?.slug} />
+              {categories.map(category => (
+                <CategoryComponent key={category.id} category={category} />
+              ))}
+            </Flex>
+          </Box>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
