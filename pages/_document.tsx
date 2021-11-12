e8f5595927fea9222d9e74a35c2fe5455fa2aa43
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-12 22:17:17 +0530

    feat(pwa): added more meta tags for better compatiblity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,15 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/logo-512x512.png" />
           <meta name="theme-color" content="#000000" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-title" content="DevKit" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="full-screen" content="yes" />
+          <meta name="browsermode" content="application" />
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
