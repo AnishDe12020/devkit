e4ff858d22291ed3a72d872dc4b3fdb7807eb463
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2021-11-13 14:34:03 +0530

    style(scrollbar): switched to a custom scrollbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,28 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { DefaultSeo } from "next-seo";
 import theme from "@/styles/theme";
 import SEO from "../seo.config";
+import { Global, css } from "@emotion/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <DefaultSeo {...SEO} />
+      <Global
+        styles={css`
+          ::-webkit-scrollbar {
+            width: 0.5rem;
+          }
+          ::-webkit-scrollbar-track {
+            background: #252525;
+          }
+          ::-webkit-scrollbar-thumb {
+            background: #494949;
+          }
+          ::-webkit-scrollbar-thumb:hover {
+            background: #757575;
+          }
+        `}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );
