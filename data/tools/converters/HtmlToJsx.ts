fc27a501df49e64aad42241d0affccd50a31665d
diganta413 <ghoshdiganta226@gmail.com>
2021-12-09 00:16:48 +0530

    feat: html to jsx converter added

---
+++ data/tools/converters/HtmlToJsx.ts
@@ -0,0 +1,12 @@
+import { Tool } from "@/data/types";
+
+const HtmlToJsx: Tool = {
+  id: 2,
+  name: "Html To Jsx",
+  description: "Convers Html To Jsx and vice versa",
+  componentFileName: "HtmlToJsx",
+  slug: "html-to-jsx",
+  categorySlug: "converters",
+};
+
+export default HtmlToJsx;
